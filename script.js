fda4660bd25d9f4f850df9461b72f07bba1feff4
Deivid Matos <deivid.matos@uni9.edu.br>
2024-10-11 11:00:34 -0300

    alter2

---
+++ script.js
@@ -0,0 +1,13 @@
+// Função para o botão "Continuar com Google"
+function loginGoogle() {
+    alert("Login com Google foi clicado!");
+    // Aqui você pode adicionar a lógica real para autenticação com o Google
+    window.location.href = "https://accounts.google.com/signin"; // Exemplo de redirecionamento para o Google
+}
+
+// Função para o botão "Continuar com Facebook"
+function loginFacebook() {
+    alert("Login com Facebook foi clicado!");
+    // Aqui você pode adicionar a lógica real para autenticação com o Facebook
+    window.location.href = "https://www.facebook.com/login"; // Exemplo de redirecionamento para o Facebook
+}
